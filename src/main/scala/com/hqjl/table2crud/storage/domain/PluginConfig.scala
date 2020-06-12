beb3dc276f01d1f0859b8962e8f07b2b825e2193
liusf <liusf12@live.com>
2020-06-12 11:58:45 +0800

    [upgrade] upgrade version of table2crud to 1.1.1

--- src/main/scala/com/hqjl/table2crud/storage/domain/PluginConfig.scala
+++ src/main/scala/com/hqjl/table2crud/storage/domain/PluginConfig.scala
@@ -1,7 +1,7 @@
 package com.hqjl.table2crud.storage.domain
 
 import com.hqjl.table2crud.domain.DBSettings
-import org.apache.commons.lang3.StringUtils
+import com.hqjl.table2crud.util.StringUtils
 
 /**
   *
